2e8d85f46f275e76c1e7594da7a5dd5908f25392
kkar2 <kkar2@LUFFY>
2026-04-21 15:22:23 +0530

    made changes

---
+++ WebApplication5/viewstate.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="viewstate.aspx.cs" Inherits="WebApplication5.viewstate" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lable1" runat="server"></asp:Label>
+            <asp:Button ID="btnsubmmit" runat="server" Text="submit" OnClick="btnsubmmit_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication5/viewstate.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication5
+{
+    public partial class viewstate : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ViewState["clickcount"] = 0;
+            }
+        }
+
+        protected void btnsubmmit_Click(object sender, EventArgs e)
+        {
+            int count = (int)ViewState["clickcount"];
+            count++;
+            ViewState["clickcount"] = count;
+            lable1.Text = "clickcount" + count;
+        }
+    }
+}
