0180667ca716eae09328eace537da62385247c0e
kkar2 <kkar2@LUFFY>
2026-04-23 12:36:20 +0530

    made changes

---
+++ WebApplication5/numberofusers.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="numberofusers.aspx.cs" Inherits="WebApplication5.numberofusers" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblcount" runat="server" Text="count"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication5/numberofusers.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication5
+{
+    public partial class numberofusers : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int currentusercount = (int)Application["usercount"];
+            lblcount.Text="Active users"+ currentusercount.ToString();
+        }
+    }
+}
