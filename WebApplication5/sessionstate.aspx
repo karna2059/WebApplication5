c5f39c9da410820515f2de4909eaeae1327ababc
kkar2 <kkar2@LUFFY>
2026-04-23 10:56:52 +0530

    made changes

---
+++ WebApplication5/sessionstate.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="sessionstate.aspx.cs" Inherits="WebApplication5.sessionstate" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblabel1" runat="server"></asp:Label>
+            <asp:Button ID="button1" runat="server" Text="submit" OnClick="button1_Click" />
+            <asp:Label ID="lblcount" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication5/sessionstate.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication5
+{
+    public partial class sessionstate : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["clickscount"] == null)
+            {
+                Session["clickscount"] = 0;
+            }
+            lblcount.Text = "clickscount" + Session["clickscount"].ToString();
+        }
+
+        protected void button1_Click(object sender, EventArgs e)
+        {
+            int count = (int)Session["clickscount"];
+            count++;
+            Session["clickscount"]=count;
+            lblcount.Text = "clikccount" + Session["clickscount"].ToString();
+        }
+    }
+}
